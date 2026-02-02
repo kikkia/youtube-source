18f7b6f5d77a82a1ca409c884e2775e30ff06fe9
kikkia <10608318+kikkia@users.noreply.github.com>
2026-02-02 22:27:27 +0900

    test

--- common/src/main/java/dev/lavalink/youtube/clients/TvHtml5Embedded.java
+++ common/src/main/java/dev/lavalink/youtube/clients/TvHtml5Embedded.java
@@ -12,10 +12,14 @@ import dev.lavalink.youtube.YoutubeAudioSourceManager;
 import dev.lavalink.youtube.clients.skeleton.StreamingNonMusicClient;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class TvHtml5Embedded extends StreamingNonMusicClient {
+    private static final Logger log = LoggerFactory.getLogger(TvHtml5Embedded.class);
+
     public static ClientConfig BASE_CONFIG = new ClientConfig()
         .withClientName("TVHTML5_SIMPLY_EMBEDDED_PLAYER")
         .withClientField("clientVersion", "2.0")
